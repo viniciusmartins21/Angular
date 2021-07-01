294a3fd465942426ce2a49006e8673291a461e47
viniciusmartins21 <viniciusmartins.12@hotmail.com>
2021-07-01 01:22:28 -0300

    Componente inicio sem consumo da API

--- blogPessoal/src/app/menu/menu.component.ts
+++ blogPessoal/src/app/menu/menu.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from './../../environments/environment.prod';
+
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
+
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  nome = environment.nome
+  foto = environment.foto
+  token = environment.token
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  sair() {
+    this.router.navigate(['/entrar'])
+    environment.token = ''
+    environment.nome = ''
+    environment.foto = ''
+    environment.id = 0
   }
 
 }
